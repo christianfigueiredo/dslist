aeebf6182a90b5383272a380d4fcbc2e02bfec3d
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2024-11-11 15:11:48 -0300

    seek games

--- dslist/src/main/java/com/devchristian/dslist/entities/Game.java
+++ dslist/src/main/java/com/devchristian/dslist/entities/Game.java
@@ -24,7 +24,11 @@ public class Game {
 	private String platform;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {}
